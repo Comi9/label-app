f0950a048b83527fa06d1795cb439dcde5d2640b
Cosmin <cosminp9@gmail.com>
2019-11-22 00:16:11 +0200

    Add dynamic performance locations with translation

--- src/locales/en/translations.js
+++ src/locales/en/translations.js
@@ -5,6 +5,29 @@ export const translationEN = {
       date: 'Date',
       time: 'Time',
       location: 'Location'
+    },
+    availableLocations: {
+      TNRS: {
+        id: 0,
+        institution: '“Radu Stanca” National Theater Sibiu',
+        alias: 'TNRS',
+        hall: 'Large Hall',
+        address: 'Corneliu Coposu Boulevard 2'
+      },
+      CAVAS: {
+        id: 1,
+        institution: 'Center for Advanced Research in Performing Arts',
+        alias: 'CAVAS',
+        hall: 'CAVAS Hall',
+        address: 'Victoriei Boulevard 5-7'
+      },
+      FdC: {
+        id: 2,
+        institution: 'Fabrica de Cultură',
+        alias: 'FdC',
+        hall: 'Faust Hall',
+        address: 'Triajului Street 1-3'
+      }
     }
   }
 }
